89242cdace9107afefd79dd1b2cbb8f0cc1052a9
Anastasia <trofimova.nastya45@yandex.ru>
2024-10-21 18:40:38 +0500

    Больше деталей

--- js/functions.js
+++ js/functions.js
@@ -15,7 +15,27 @@ function findNumbers (line) {
     }
   }
   if (count === ''){
-    return 'NaN';
+    return NaN;
   }
-  return count;
+  return Number(count);
 }
+
+
+console.log(checkLength('проверяемая строка', 20));
+console.log(checkLength('проверяемая строка', 18));
+console.log(checkLength('проверяемая строка', 10));
+
+
+console.log(checkPalidrome('топот'));
+console.log(checkPalidrome('ДовОд'));
+console.log(checkPalidrome('Кекс'));
+
+
+console.log(findNumbers('2023 год'));
+console.log(findNumbers('ECMAScript 2022'));
+console.log(findNumbers('1 кефир, 0.5 батона'));
+console.log(findNumbers('агент 007'));
+console.log(findNumbers('а я томат'));
+console.log(findNumbers(2023));
+console.log(findNumbers(-1));
+console.log(findNumbers(1.5));

d2ec02a36658ae3c6df1af0faeff51b37cea6630
Anastasia <trofimova.nastya45@yandex.ru>
2024-10-26 17:54:50 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -20,22 +20,26 @@ function findNumbers (line) {
   return Number(count);
 }
 
-
 checkLength('проверяемая строка', 20);
-checkLength('проверяемая строка', 18);
-checkLength('проверяемая строка', 10);
-
-
 checkPalidrome('топот');
-checkPalidrome('ДовОд');
-checkPalidrome('Кекс');
-
-
 findNumbers('2023 год');
-findNumbers('ECMAScript 2022');
-findNumbers('1 кефир, 0.5 батона');
-findNumbers('агент 007');
-findNumbers('а я томат');
-findNumbers(2023);
-findNumbers(-1);
-findNumbers(1.5);
+
+
+function convertToMinutes(data) {
+  const time = data.split(':');
+  return Number(time[0]) * 60 + Number(time[1]);
+}
+
+function meetingDuringHours(startWork, endWork, startmeeting, duration) {
+
+  const startWorkM = convertToMinutes(startWork);
+  const endWorkM = convertToMinutes(endWork);
+  const startmeetingM = convertToMinutes(startmeeting);
+  return startWorkM <= startmeetingM && startmeetingM + duration <= endWorkM;
+}
+
+meetingDuringHours('08:00', '17:30', '14:00', 90); // true
+meetingDuringHours('8:0', '10:0', '8:0', 120);     // true
+meetingDuringHours('08:00', '14:30', '14:00', 90); // false
+meetingDuringHours('14:00', '17:30', '08:0', 90);  // false
+meetingDuringHours('8:00', '17:30', '08:00', 900); // false

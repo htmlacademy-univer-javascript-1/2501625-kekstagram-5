681249fd4cb481de7acb1d5d028e4190ff6a6588
Anastasia <trofimova.nastya45@yandex.ru>
2024-10-26 17:57:33 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -38,8 +38,8 @@ function meetingDuringHours(startWork, endWork, startmeeting, duration) {
   return startWorkM <= startmeetingM && startmeetingM + duration <= endWorkM;
 }
 
-meetingDuringHours('08:00', '17:30', '14:00', 90); // true
-meetingDuringHours('8:0', '10:0', '8:0', 120);     // true
-meetingDuringHours('08:00', '14:30', '14:00', 90); // false
-meetingDuringHours('14:00', '17:30', '08:0', 90);  // false
-meetingDuringHours('8:00', '17:30', '08:00', 900); // false
+meetingDuringHours('08:00', '17:30', '14:00', 90);
+meetingDuringHours('8:0', '10:0', '8:0', 120);
+meetingDuringHours('08:00', '14:30', '14:00', 90);
+meetingDuringHours('14:00', '17:30', '08:0', 90);
+meetingDuringHours('8:00', '17:30', '08:00', 900);
